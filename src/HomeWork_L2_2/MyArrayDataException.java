d668cdc80a4f571b70ba5be783ce9857bc0812b0
Vladislav <infemos@mail.ru>
2020-05-20 16:30:42 +0800

    Home Work Lesson 2

---
+++ src/HomeWork_L2_2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package HomeWork_L2_2;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/HomeWork_L2_2/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package HomeWork_L2_2;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+
+    }
+}
